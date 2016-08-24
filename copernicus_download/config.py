93a2ba6efb964872f8e2ec738c8d42dba8c8feb8
Jonas <josl@dhi-gras.com>
2016-08-24 15:02:20 +0200

    include WB

--- copernicus_download/config.py
+++ copernicus_download/config.py
@@ -3,13 +3,16 @@ import datetime
 datapool_url = 'http://land.copernicus.vgt.vito.be/PDF/datapool/'
 
 product_base_urls = {
+        'Water_Bodies_Global': 'Water/Water_Bodies/Water_Bodies_Global_V2',
         'SWI': 'Water/Soil_Water/SWI_V3',
         'SWI_10': 'Water/Soil_Water/SWI10_V3'}
 
 timestep_days = {
+        'Water_Bodies_Global': 10,
         'SWI': 1,
         'SWI10': 10}
 
 timeseries_startdate = {
+        'Water_Bodies_Global': datetime.datetime(2014, 1, 1),
         'SWI': datetime.datetime(2007, 8, 1),
         'SWI10': datetime.datetime(2007, 8, 1)}
--- copernicus_download/extract.py
+++ copernicus_download/extract.py
@@ -11,3 +11,15 @@ def extract_h5(zipfname, outdir):
     if h5fname is None:
         raise IOError('Unable to find \'h5\' data in zip file {}.'.format(zipfname))
     return h5fname
+
+
+def extract_tiff(zipfname, outdir):
+    tifffname = None
+    with ZipFile(zipfname, 'r') as zf:
+        for member in zf.infolist():
+            if member.filename.endswith('.tiff') and 'QUAL' not in member.filename:
+                tifffname = zf.extract(member, outdir)
+                break
+    if tifffname is None:
+        raise IOError('Unable to find \'tiff\' data in zip file {}.'.format(zipfname))
+    return tifffname
