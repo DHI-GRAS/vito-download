47c8edb35b1ed92692734214e0cce43e31c2a6a5
Jonas <josl@dhi-gras.com>
2016-08-25 08:40:05 +0200

    bug fix KeyError SWI10 for #92

--- copernicus_download/config.py
+++ copernicus_download/config.py
@@ -5,7 +5,7 @@ datapool_url = 'http://land.copernicus.vgt.vito.be/PDF/datapool/'
 product_base_urls = {
         'Water_Bodies_Global': 'Water/Water_Bodies/Water_Bodies_Global_V2',
         'SWI': 'Water/Soil_Water/SWI_V3',
-        'SWI_10': 'Water/Soil_Water/SWI10_V3'}
+        'SWI10': 'Water/Soil_Water/SWI10_V3'}
 
 timestep_days = {
         'Water_Bodies_Global': 10,
