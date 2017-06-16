5918ba37b761b7abd0985a3fd67206f8668d3fa3
Jonas <josl@dhi-gras.com>
2017-06-16 20:26:10 +0200

    new mail

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version='0.1',
     description='Download from land.copernicus.vgt.vito.be/PDF/datapool',
     author='Jonas Solvsteen',
-    author_email='josl@dhi-gras.com',
+    author_email='josl@dhigroup.com',
     url='https://www.dhi-gras.com',
     packages=find_packages(),
-    install_requires=['wget_provider'],
-    dependency_links=['https://github.com/DHI-GRAS/wget_provider.git'])
+    install_requires=[
+        'wget_provider'],
+    dependency_links=[
+        'https://github.com/DHI-GRAS/wget_provider.git'])
