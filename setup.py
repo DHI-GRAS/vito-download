a3fa2256f60a06bbe6c6aa3e5154e9b84f0d2d33
Jonas Sølvsteen <j08lue@gmail.com>
2017-10-12 12:02:45 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vito_download',
-    version='1.0',
+    version='2.0',
     description='Download from land.copernicus.vgt.vito.be/PDF/datapool',
     author='Jonas Solvsteen',
     author_email='josl@dhigroup.com',
