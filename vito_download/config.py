b2f787ed88f992901d33f6aa75578119ede681c1
LaustFaerch <laust.faerch@gmail.com>
2020-08-28 09:06:19 +0200

    updated proba-v urls

--- vito_download/config.py
+++ vito_download/config.py
@@ -5,8 +5,8 @@ product_url = {
     'SWI': 'http://land.copernicus.vgt.vito.be/PDF/datapool/Vegetation/Soil_Water_Index/Daily_SWI_12.5km_Global_V3',
     'SWI10': 'http://land.copernicus.vgt.vito.be/PDF/datapool/Vegetation/Soil_Water_Index/10-daily_SWI_12.5km_Global_V3',
     'SSM': 'http://land.copernicus.vgt.vito.be/PDF/datapool/Vegetation/Surface_Soil_Moisture/BioPar_SSM1km_V1_Global',
-    'Proba-V-NDVI': 'https://www.vito-eodata.be/PDF/datapool/Free_Data/PROBA-V_100m/S1_TOC_NDVI_100_m__C1',
-    'Proba-V': 'https://www.vito-eodata.be/PDF/datapool/Free_Data/PROBA-V_100m/S1_TOC_100_m_C1'
+    'Proba-V-NDVI': 'https://www.vito-eodata.be/PDF/datapool/Free_Data/PROBA-V_100m/S5_TOC_NDVI_100_m_C1',
+    'Proba-V': 'https://www.vito-eodata.be/PDF/datapool/Free_Data/PROBA-V_100m/S5_TOC_100_m_C1'
 }
 
 timestep_days = {
